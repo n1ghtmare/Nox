b40b6cf3b32284ff592314e211234ee01e124c5c
Dimitar Dimitrov <dimitrov2k@gmail.com>
2013-04-02 20:50:45 +0400

    Initial Commit - After project creation

---
+++ Nox.Tests/Something.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Nox.Tests
+{
+    public class Something
+    {
+        [Test]
+        public void DynamicObject_CallsSomething()
+        {
+            // Arrange
+            var table = new Employees();
+
+
+            // Act
+            table.Insert(new {Id = "TEST", FirstName = "Dimitar", LastName = "Dimitrov"});
+
+            // Assert
+        }
+    }
+}
