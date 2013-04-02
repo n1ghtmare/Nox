236eeef60f95b60f98a1c5e55fb1b4d48b210e0e
Dimitar Dimitrov <dimitrov2k@gmail.com>
2013-04-02 22:05:02 +0400

    First test ... lot to do

---
+++ Nox.Tests/Fetch.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Dynamic;
+using System.Linq.Expressions;
+using Moq;
+using NUnit.Framework;
+
+namespace Nox.Tests
+{
+    public class Get
+    {
+        [Test]
+        public void GivenAWhereClauseAndParameters_CallsProviderExecuteQueryWithCorrectSqlQuery()
+        {
+            // Arrange
+            var provider = new Mock<INoxProvider>();
+            var nox = new Nox(provider.Object);
+
+            // Act
+            IEnumerable<Employee> employeeList = nox.Get<Employee>(where: "FirstName=@FirstName and LastName=@LastName",
+                                                                   param:
+                                                                       new
+                                                                           {
+                                                                               FirstName = "Dimitar",
+                                                                               LastName = "Dimitrov"
+                                                                           });
+
+            
+            // Assert
+            provider.Verify(
+                x => x.ExecuteQuery("SELECT * FROM Employee WHERE FirstName='Dimitar' and LastName='Dimitrov'"),
+                Times.Once());
+        }
+    }
+
+
+    public class Employee
+    {
+        
+    }
+
+    public interface INoxProvider
+    {
+//        string ConnectionString { get; set; }
+        object ExecuteQuery(string sql);
+    }
+
+
+    public class Nox
+    {
+        private INoxProvider _provider;
+
+        public Nox(INoxProvider provider)
+        {
+            _provider = provider;
+        }
+        public IEnumerable<T> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Get<T>(string where = "", object param = null)
+        {
+            string sqlQuery = "SELECT * FROM {0} WHERE {1}";
+            // Build select query -> replace parameters as well here ...
+//            throw new NotImplementedException();
+
+            return null;
+        }
+    }
+}
--- Nox.Tests/Properties/AssemblyInfo.cs
+++ Nox.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nox.Tests")]
+[assembly: AssemblyTitle("NoxProvider.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Nox.Tests")]
+[assembly: AssemblyProduct("NoxProvider.Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
